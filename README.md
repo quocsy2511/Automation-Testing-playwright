2c9b36df6c4501384080e06e5c22924a6d0e3142
nguyễn quốc sỹ <quocsy2511@gmail.com>
2024-12-13 18:30:32 +0700

    refactor payment

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Testing-playwright
 
 run test with html : npx playwright test --config=playwright.config.ts --project=chromium --reporter=html
+
 run test : npm run tests:chrome
+
+run test with html : npm run tests:e2e -- --headed
--- tests/e2e/e2e-payment.spec.ts
+++ tests/e2e/e2e-payment.spec.ts
@@ -2,11 +2,13 @@ import { expect, test } from '@playwright/test'
 import { HomePage } from '../../page-objects/HomePage'
 import { LoginPage } from '../../page-objects/LoginPage'
 import { Navbar, TabNameInitial } from '../../page-objects/components/Navbar'
+import { PaymentPage } from '../../page-objects/PaymentPage'
 
-test.describe('Payment bills', () => {
+test.describe.only('Payment bills', () => {
   let homePage: HomePage
   let loginPage: LoginPage
   let navbar: Navbar
+  let paymentPage: PaymentPage
 
   // test.beforeEach(async ({ page }) => {
   //   await page.goto('http://zero.webappsecurity.com/index.html')
@@ -19,7 +21,6 @@ test.describe('Payment bills', () => {
 
   // test('should saved payees', async ({ page }) => {
   //   // await page.click('#pay_bills_tab')
-  //   navbar.clickActiveTab({ tabName: TabNameInitial.PAY_BILL })
 
   //   await page.selectOption('#sp_payee', 'apple')
   //   await page.click('#sp_get_payee_details')
@@ -45,33 +46,27 @@ test.describe('Payment bills', () => {
     loginPage = new LoginPage(page)
     homePage = new HomePage(page)
     navbar = new Navbar(page)
+    paymentPage = new PaymentPage(page)
 
     await homePage.visit()
     await homePage.clickOnSignIn()
     await loginPage.login({ username: 'username', password: 'password' })
     await loginPage.reloadPage()
+    await navbar.clickActiveTab({ tabName: TabNameInitial.PAY_BILL })
   })
 
   test('should saved payees', async ({ page }) => {
-    // await page.click('#pay_bills_tab')
-    navbar.clickActiveTab({ tabName: TabNameInitial.PAY_BILL })
+    await paymentPage.fillForm({
+      account: '2',
+      amount: '100',
+      date: '2024-12-03',
+      description: '123456789',
+      payee: 'apple',
+    })
 
-    await page.selectOption('#sp_payee', 'apple')
-    await page.click('#sp_get_payee_details')
-    const payeeDetails = await page.locator('#sp_payee_details')
-
-    await expect(payeeDetails).toBeVisible()
-
-    await page.selectOption('#sp_account', '2')
-    await page.fill('#sp_amount', '100')
-    await page.fill('#sp_date', '2024-12-03')
-    await page.fill('#sp_description', '123456789')
-
-    await page.click('#pay_saved_payees')
-
-    const success = await page.locator('#alert_content > span')
-    await expect(success).toContainText(
-      'The payment was successfully submitted.',
-    )
+    await paymentPage.clickPayeeDetail()
+    await paymentPage.assertPayeeDetail()
+    await paymentPage.submitForm()
+    await paymentPage.assertSuccess()
   })
 })
