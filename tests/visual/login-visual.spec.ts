d961c19b2d5c5d702c7c7ebd10b7e6f72640ced2
nguyễn quốc sỹ <quocsy2511@gmail.com>
2024-12-16 12:09:56 +0700

    testing api

--- tests/visual/login-visual.spec.ts
+++ tests/visual/login-visual.spec.ts
@@ -2,7 +2,7 @@ import test from '@playwright/test'
 import { HomePage } from '../../page-objects/HomePage'
 import { LoginPage } from '../../page-objects/LoginPage'
 
-test.describe.only('login-flow', () => {
+test.describe('login-flow', () => {
   let homePage: HomePage
   let loginPage: LoginPage
 
