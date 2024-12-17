8749cb251905e23de085ac33a63cd128435cb1af
nguyễn quốc sỹ <quocsy2511@gmail.com>
2024-12-17 14:45:40 +0700

    setup codecept tool

--- cucumber.js
+++
@@ -1,8 +0,0 @@
-const common = `
---require setup/assertions.js
---require setup/hook.js
---require step-definitions/**/*.step.js
-`
-module.exports = {
-  default: `${common} features/**/*.feature`,
-}
--- setup/assertions.js
+++
@@ -1,9 +0,0 @@
-//Chai là 1 thư viện assertion đóng vai trò như một công cụ để thực hiện các so sánh hoặc xác minh kết quả trong các bước kiểm thử
-async function setupChai() {
-  const chai = await import('chai')
-  global.expect = chai.expect
-  global.assert = chai.assert
-  global.should = chai.should
-}
-
-setupChai()
